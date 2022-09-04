549c186cadc28fdcd7cf6cd6fd8b212f548cf8fa
Amanda Hernández <avhernan@terpmail.umd.edu>
2022-09-04 18:04:57 -0400

    scores

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score')
   const width = 10;
 
   console.log(squares);
