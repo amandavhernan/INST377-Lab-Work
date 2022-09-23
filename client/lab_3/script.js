24bf417547eea1fde07e066f25130ebc401bfbdc
Amanda Hernández <avhernan@terpmail.umd.edu>
2022-09-23 14:26:41 -0400

    fixed prev function

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -5,6 +5,7 @@
   This file contains parts of a simple script to make your carousel work.
   Please feel free to edit away - the main version of this with all the notes is safely stored elsewhere
 */
+
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
 let slidePosition = 0;
@@ -28,28 +29,20 @@ function updateSlidePosition() {
 }
 
 function moveToNextSlide() {
-  /*
-    add an if statement here that checks
-    if you're already at the max number of slides
-    and if so, sets your slidePosition to the first index of an array
-    if not, set the slidePosition to the current position plus one
-  */
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition += 1;
   }
-
   updateSlidePosition(); // this is how you call a function within a function
 }
+
 function moveToPrevSlide() {
-  // add your code in here for when you click the "prev" button
-  /*
-    add an if statement here that checks
-    if you're already at the first index position for an array
-    and if so, sets your slidePosition to the last slide position in totalSlides
-    if not, set the slidePosition to the current position minus one
-  */
+  if (slidePosition <= 0) {
+    slidePosition = 3;
+  } else {
+    slidePosition -= 1;
+  }
   updateSlidePosition();
 }
 
