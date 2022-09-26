cb130a66255f4679b23ba2bb16b07c140d62f23a
Amanda Hernández <avhernan@terpmail.umd.edu>
2022-09-25 20:18:01 -0400

    update

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -40,4 +40,24 @@ document.addEventListener('DomContentLoaded', () => {
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3]
   ];
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 4;
+  let currentRotation = 0;
+
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let current = theTetrominoes[random][currentRotation];
+
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+  }
+
+  function undraw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove.tetromino;
+    });
+  }
 });
